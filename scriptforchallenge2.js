ea8767bd3c1d035be5f7a5c339220eee99b762cb
markantao <35659231+markantao@users.noreply.github.com>
2020-06-07 22:05:01 -0400

    Add files via upload

---
+++ scriptforchallenge2.js
@@ -0,0 +1,9 @@
+function generateCat() { 
+    var image = document.createElement('img'); 
+    var div = document.getElementById('flex-cat-gen') // Give id to flex-box-container-2. 
+    image.src= "https://media.giphy.com/media/5LU6ZcEGBbhVS/giphy.gif" 
+    div.appendChild(image);  
+} 
+
+
+
